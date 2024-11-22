2c0721ddfcc09da08f70e782895426490ce6e678
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-22 14:33:44 -0400

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -1,12 +1,11 @@
 package classroom;
 
 public class Grupo {
-
-    public Persona[] estudiantes;
-    public Persona profesor;
-    public Asignatura asignatura;
-    public final int codigo;
-    public String horario;
+    Persona[] estudiantes;
+    Persona profesor;
+    Asignatura asignatura;
+    int codigo;
+    String horario;
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         this.estudiantes = estudiantes;
@@ -16,26 +15,11 @@ public class Grupo {
         this.horario = horario;
     }
 
-    public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
-    }
-
-    public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
-        this(estudiantes, profesor, asignatura, 0, "");
-    }
-
-    public void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
-        for (int i = 0; i < estudiantes.length; i++) {
-            if (estudiantes[i] != null && estudiantes[i].getCedula() == estudianteViejo.getCedula()) {
-                estudiantes[i] = estudianteNuevo;
-                break;
-            }
-        }
-    }
-
-    public void cambiarEstudiante(int indice, Persona estudiante) {
-        if (indice >= 0 && indice < estudiantes.length) {
-            estudiantes[indice] = estudiante;
-        }
+    public Grupo(int numEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
+        this.estudiantes = new Persona[numEstudiantes];
+        this.profesor = profesor;
+        this.asignatura = asignatura;
+        this.codigo = codigo;
+        this.horario = horario;
     }
 }
