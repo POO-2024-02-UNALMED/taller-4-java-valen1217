284352c0663f79bc8e31d792c510478d9ba1e992
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-22 15:08:51 -0400

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,8 +1,8 @@
 package classroom;
 
 public enum Tipo {
-    DISCIPLINAR(10, "Disciplina"),
-    FUNDAMENTACION(20, "Fundamentación"),
+    DISCIPLINAR(10, "Disciplina"), 
+    FUNDAMENTACION(20, "Fundamentación"), 
     ELECTIVA(30, "Electiva");
 
     private int codigo;
@@ -11,5 +11,13 @@ public enum Tipo {
     Tipo(int codigo, String nombre) {
         this.codigo = codigo;
         this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 }
