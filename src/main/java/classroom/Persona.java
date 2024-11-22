456dfb06c5023b5814088cc7ad4a721ae14bef01
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-22 13:57:29 -0400

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,14 +1,12 @@
 package classroom;
 
 public class Persona {
-
     final long cedula;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -16,24 +14,23 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
+        this(cedula, nombre);
     }
 
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
+        this(cedula, "");
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
+        this(0, nombre);
     }
-    
+
+    public Persona() {
+        this(0, "");
+    }
+
     public long getCedula() {
         return cedula;
     }
@@ -46,3 +43,4 @@ public class Persona {
         this.nombre = nombre;
     }
 }
+
