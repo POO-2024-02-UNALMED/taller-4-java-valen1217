2ba9380b5122b808ec807fef59ef301fa449efbf
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-22 15:23:37 -0400

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -4,24 +4,24 @@ public class Persona {
     private int cedula;
     private String nombre;
 
-    public Persona() {
-        this.cedula = 0;
-        this.nombre = "Sin nombre";
-    }
-
     public Persona(int cedula) {
         this.cedula = cedula;
         this.nombre = "Sin nombre";
     }
 
-    public Persona(String nombre) {
-        this.cedula = 1;
+    public Persona(int cedula, String nombre) {
+        this.cedula = cedula;
         this.nombre = nombre;
     }
 
-    public Persona(String nombre, int cedula) {
-        this.cedula = cedula;
+    public Persona(String nombre) {
+        this.cedula = 0;
         this.nombre = nombre;
+    }
+
+    public Persona() {
+        this.cedula = 0;
+        this.nombre = "Sin nombre";
     }
 
     public int getCedula() {
