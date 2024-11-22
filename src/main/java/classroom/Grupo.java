0f0c238020cec322c23ced24c702d1fc0ff2ad5f
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-22 14:54:24 -0400

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
-    public int codigo;  // No usar "final" para permitir la asignación
+    public final int codigo;
     public String horario;
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -35,7 +35,7 @@ public class Grupo {
             }
         }
     }
-    
+
     void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
     }
