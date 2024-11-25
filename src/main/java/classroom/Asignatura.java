5b9d5d9c61c63814ed303898b113af4abd7b7363
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-25 17:15:46 -0400

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,39 +1,39 @@
 package classroom;
 
 public class Asignatura {
-   public String nombre;
+    public String nombre;
     public Tipo tipo;
     public int codigoExterno;
     public int codigoInterno;
-   
+
     public Asignatura() {
         this.nombre = "Sin nombre"; 
         this.tipo = Tipo.FUNDAMENTACION;  
         this.codigoExterno = 0;  
         this.codigoInterno = 0;
     }
-    
+
     public Asignatura(String nombre, Tipo tipo) {
         this.nombre = (nombre != null && !nombre.isEmpty()) ? nombre : "Sin nombre";  
         this.tipo = tipo != null ? tipo : Tipo.FUNDAMENTACION; 
         this.codigoExterno = 0;  
         this.codigoInterno = 0;  
     }
-  
+
     public Asignatura(int codigoExterno, Tipo tipo) {
         this.nombre = "Sin nombre";  
         this.tipo = tipo != null ? tipo : Tipo.FUNDAMENTACION;  
         this.codigoExterno = codigoExterno; 
         this.codigoInterno = 0; 
     }
-    
+
     public Asignatura(String nombre, int codigoExterno, int codigoInterno, Tipo tipo) {
         this.nombre = (nombre != null && !nombre.isEmpty()) ? nombre : "Sin nombre"; 
         this.codigoExterno = codigoExterno; 
         this.codigoInterno = codigoInterno;
-        this.tipo = tipo != null ? tipo : Tipo.FUNDAMENTACION;  
+        this.tipo = tipo != null ? tipo : Tipo.FUNDAMENTACION; 
     }
-    
+
     public void cambiarDatos(String nuevoNombre) {
         this.nombre = nuevoNombre;
     }
@@ -42,7 +42,6 @@ public class Asignatura {
         this.codigoExterno = nuevoCodigo;
     }
 
-  
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
